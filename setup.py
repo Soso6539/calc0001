1926482129b34a732261cd5597aaa4f09a571827
Soso6539 <162390471+Soso6539@users.noreply.github.com>
2024-04-21 07:12:11 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True, 
+  name='mycalc0001',
+  version='0.0.1',
+  description='oss-dev my calculator 0001',
+  author='sohyeon',
+  author_email='sohyeon.ac@gmail.com',
+  url = "https://github.com/Soso6539/calc0001",
+  download_url = "https://github.com/Soso6539/calc0001/archive/refs/tags/v0.0.1.zip",
+  install_requires=['pytest'],
+  long_description = 'oss-dev calculator python module',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+      "Programmming Language :: Python :: 3",
+      "Operating System :: OS Independent",
+  ],
+)
+  
